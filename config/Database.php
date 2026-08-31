48158b257b0b387421db90fb1d859eaba950d850
MiguelsHere <ooomonteiro42@gmail.com>
2026-08-31 06:25:34 +0100

    Planeamento e criação da config da Base de Dados

---
+++ config/Database.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database
+{
+    private $host = "localhost";
+    private $db_name = "workout_db";
+    private $username = "workout_admin";
+    private $password = "123";
+    public $conn;
+
+    public function getConnection()
+    {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->conn->exec("set names utf8");
+        } catch (PDOException $exception) {
+            echo "Erro de conexão: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
